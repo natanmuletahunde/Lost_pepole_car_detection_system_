b220bb2b49096c54ee88e0d38dd5960e9b2a5467
pivnati <natanmuleta911@gmail.com>
2026-04-17 20:10:02 -1200

    remove Swagger UI integration and related files from the project

--- src/frontend/app/api/docs/route.tsx
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from "next/server";
-import swaggerJson from "../../../swagger.json";
-
-export const dynamic = "force-dynamic";
-
-export const GET = () => NextResponse.json(swaggerJson);
